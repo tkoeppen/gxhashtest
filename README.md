8b314a3e6f45ff2b040916cca00ded78a3db7af5
Thomas Koeppen <tkoeppen@gmail.com>
2023-12-14 13:34:03 +0100

    better string iter function and compare gxhash64 and gxhash128

--- README.md
+++ README.md
@@ -1,32 +1,43 @@
 # gxhashtest
+
 compare gxhash with DefaultHasher in Rust
 
 We want to test here GxHash (a non-cryptographic hashing algorithm) from Olivier Giniaux (published in September 2023) [1].
+Also we want to find out, if string iter or string truncate is faster for our use case.
 
 Run with:
 
 ```
-cargo test -- --nocapture
+cargo test --release -- --nocapture
 ```
+
+(Note: use --release to have faster results)
 
 Example output:
 
 ```
-uuid: 904ebd40-74e9-4e23-84ea-517501e7abdb, hashed_uid: 14441696786189459178, len: 20, hash took: 288 ms, total took: 674 ms
-uuid: 904ebd40-74e9-4e23-84ea-517501e7abdb, gx_hashed_uid: 3782867463144427854, len: 20, hash took: 293 ms, total took: 676 ms
+running 1 test
+uuid: f6c45ae9-4486-4038-ab8e-817c5ca11059, hashed_uid: 836249621649601198, len: 18, hash took: 483 ms
+uuid: f6c45ae9-4486-4038-ab8e-817c5ca11059, gx_hashed64_uid v1 (truncate): 14770598964675748883, len: 20, hash took: 368 ms, total took: 400 ms
+uuid: f6c45ae9-4486-4038-ab8e-817c5ca11059, gx_hashed64_uid v2 (fill_iter): 14770598964675748883, len: 20, hash took: 369 ms, total took: 809 ms
+uuid: f6c45ae9-4486-4038-ab8e-817c5ca11059, gx_hash_uid128 v1 (truncate): 30496146084139209394, len: 20, hash took: 583 ms, total took: 615 ms
+uuid: f6c45ae9-4486-4038-ab8e-817c5ca11059, gx_hash_uid128 v2 (fill_iter): 30496146084139209394, len: 20, hash took: 577 ms, total took: 1015 ms
 ```
 
-The program creates a uuid and has 3 helper functions:
-1. hash_uuid to hash a uuid with DefaultHasher
-2. gxhash_uuid to hash a uuid with gxhash
-3. a string limit function to limit the string to a fixed length of 20 characters
+The program creates a uuid and has 5 helper functions:
 
-The programm calls the both hash and limit function 1 million times to measure the time spent.
+1. hash_uuid to hash a uuid with DefaultHasher
+2. gxhash_uuid64 to hash a uuid with gxhash64
+3. gxhash_uuid128 to hash a uuid with gxhash128
+4. a string iter/fill function to read a string and fill it with '1' chars at the end, if the string is shorter than 20 chars
+5. a string truncate function to limit the string to a fixed length of 20 characters
+
+The program calls both hash and limit function 1 billion times to measure the time spent.
 
 The first "took ms" number is the time spent for the hashing function and the total time includes the limitation to 20 chars.
 
-The goal of the function is, that we get a fixed length shorter uuid (similar what Google does).
+The goal of the program is, that we get shorter uuids with numbers only and a fixed length  (similar what Google does).
 
+[1] <https://github.com/ogxd/gxhash>
 
-[1] https://github.com/ogxd/gxhash
-[2] https://www.reddit.com/r/rust/comments/17xgn0t/gxhash_a_new_extremely_fast_and_robust_hashing/?rdt=51749
+[2] <https://www.reddit.com/r/rust/comments/17xgn0t/gxhash_a_new_extremely_fast_and_robust_hashing/?rdt=51749>
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-use gxhash::gxhash64;
+use gxhash::{gxhash128, gxhash64};
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
+use std::iter::repeat;
 use uuid::Uuid;
 
-fn limit_variable_string_length(value: String) -> String {
-    let mut array_tmp = [0u8; 20];
-    array_tmp[..value.len()].copy_from_slice(value.as_bytes());
-    // convert the array of bytes to a string and return it
-    array_tmp.iter().map(|&i| char::from(i)).collect::<String>().replace('\x00', "1")
+// limit the length of a string to 20 chars
+// the result string is shorter than 20 chars, when the input string is shorter than 20 chars
+fn limit_variable_string_length_v1(mut value: String) -> String {
+    value.truncate(20);
+    value
+}
+
+// limit the length of a string to 20 chars
+// the result string is always 20 chars long
+fn limit_variable_string_length_v2(value: String) -> String {
+    value.chars().chain(repeat('1')).take(20).collect()
 }
 
 fn hash_uid(uid: Uuid) -> String {
@@ -20,11 +27,18 @@ fn hash_uid(uid: Uuid) -> String {
 /// replaces DefaultHasher with https://github.com/ogxd/gxhash
 /// Uuid is a generated unique id, e.g. c4d33c4e-74ac-4952-ba43-d454be176bd6
 /// the function returns a string with a fixed length of 20 chars, e.g. 13341702002454322048
-fn gx_hash_uid(uid: Uuid) -> String {
+fn gx_hash_uid64(uid: Uuid) -> String {
     // convert Uuid to bytes
     let bytes: &[u8] = uid.as_bytes();
     // use gxhash64 to hash, this produces a 19 or 20 chars long string
     gxhash64(bytes, 1234).to_string()
+}
+
+fn gx_hash_uid128(uid: Uuid) -> String {
+    // convert Uuid to bytes
+    let bytes: &[u8] = uid.as_bytes();
+    // use gxhash128 to hash, this produces a 39 or 40 chars long string
+    gxhash128(bytes, 1234).to_string()
 }
 
 fn main() {
@@ -35,7 +49,10 @@ fn main() {
 mod tests {
     use std::time::Instant;
 
-    use crate::{gx_hash_uid, hash_uid, limit_variable_string_length};
+    use crate::{
+        gx_hash_uid128, gx_hash_uid64, hash_uid, limit_variable_string_length_v1,
+        limit_variable_string_length_v2,
+    };
     use uuid::Uuid;
 
     #[test]
@@ -43,48 +60,113 @@ mod tests {
         let uid = Uuid::new_v4();
         let mut hashed_uid = "".to_string();
 
+        // 1st test with default hasher
         let start_time = Instant::now();
-        for _ in 1..1000000 {
+        for _ in 1..10000000 {
             hashed_uid = hash_uid(uid);
         }
         let duration_hash = start_time.elapsed();
 
-        for _ in 1..1000000 {
-            hashed_uid = limit_variable_string_length(hashed_uid);
-        }
-        let duration = start_time.elapsed();
-
         println!(
-            "uuid: {}, hashed_uid: {}, len: {}, hash took: {:?} ms, total took: {:?} ms",
+            "uuid: {}, hashed_uid: {}, len: {}, hash took: {:?} ms",
             uid,
             hashed_uid,
             hashed_uid.len(),
-            duration_hash.as_millis(),
-            duration.as_millis()
+            duration_hash.as_millis()
         );
 
+        // 2nd test with gx_hash_uid64 and string truncate
         let start_time = Instant::now();
-        let mut gx_hashed_uid = "".to_string();
-        for _ in 1..1000000 {
-            gx_hashed_uid = gx_hash_uid(uid);
+        let mut gx_hashed64_uid = "".to_string();
+        for _ in 1..10000000 {
+            gx_hashed64_uid = gx_hash_uid64(uid);
         }
         let duration_hash = start_time.elapsed();
 
-        for _ in 1..1000000 {
-            gx_hashed_uid = limit_variable_string_length(gx_hashed_uid);
+        for _ in 1..10000000 {
+            gx_hashed64_uid = limit_variable_string_length_v1(gx_hashed64_uid);
         }
         let duration = start_time.elapsed();
 
         println!(
-            "uuid: {}, gx_hashed_uid: {}, len: {}, hash took: {:?} ms, total took: {:?} ms",
+            "uuid: {}, gx_hashed64_uid v1 (truncate): {}, len: {}, hash took: {:?} ms, total took: {:?} ms",
             uid,
-            gx_hashed_uid,
-            gx_hashed_uid.len(),
+            gx_hashed64_uid,
+            gx_hashed64_uid.len(),
             duration_hash.as_millis(),
             duration.as_millis()
         );
 
-        assert_eq!(gx_hashed_uid.len(), 20);
+        // 3rd test with gx_hash_uid64 and string fill_iter
+        let start_time = Instant::now();
+        let mut gx_hashed64_uid = "".to_string();
+        for _ in 1..10000000 {
+            gx_hashed64_uid = gx_hash_uid64(uid);
+        }
+        let duration_hash = start_time.elapsed();
+
+        for _ in 1..10000000 {
+            gx_hashed64_uid = limit_variable_string_length_v2(gx_hashed64_uid);
+        }
+        let duration = start_time.elapsed();
+
+        println!(
+            "uuid: {}, gx_hashed64_uid v2 (fill_iter): {}, len: {}, hash took: {:?} ms, total took: {:?} ms",
+            uid,
+            gx_hashed64_uid,
+            gx_hashed64_uid.len(),
+            duration_hash.as_millis(),
+            duration.as_millis()
+        );
+
+        // 4th test with gx_hash_uid128 and string truncate
+        // this was the fastest test
+        let start_time = Instant::now();
+        let mut gx_hashed128_uid = "".to_string();
+        for _ in 1..10000000 {
+            gx_hashed128_uid = gx_hash_uid128(uid);
+        }
+        let duration_hash = start_time.elapsed();
+
+        for _ in 1..10000000 {
+            gx_hashed128_uid = limit_variable_string_length_v1(gx_hashed128_uid);
+        }
+        let duration = start_time.elapsed();
+
+        println!(
+            "uuid: {}, gx_hash_uid128 v1 (truncate): {}, len: {}, hash took: {:?} ms, total took: {:?} ms",
+            uid,
+            gx_hashed128_uid,
+            gx_hashed128_uid.len(),
+            duration_hash.as_millis(),
+            duration.as_millis()
+        );
+
+        // 5th test with gx_hash_uid128 and string fill_iter
+        let start_time = Instant::now();
+        let mut gx_hashed128_uid = "".to_string();
+        for _ in 1..10000000 {
+            gx_hashed128_uid = gx_hash_uid128(uid);
+        }
+        let duration_hash = start_time.elapsed();
+
+        for _ in 1..10000000 {
+            gx_hashed128_uid = limit_variable_string_length_v2(gx_hashed128_uid);
+        }
+        let duration = start_time.elapsed();
+
+        println!(
+            "uuid: {}, gx_hash_uid128 v2 (fill_iter): {}, len: {}, hash took: {:?} ms, total took: {:?} ms",
+            uid,
+            gx_hashed128_uid,
+            gx_hashed128_uid.len(),
+            duration_hash.as_millis(),
+            duration.as_millis()
+        );
+
+        assert_eq!(gx_hashed64_uid.len(), 20);
+        assert_eq!(gx_hashed128_uid.len(), 20);
+        // this assert may break, because we have no guarantee that the result string is always 20 chars long
         assert_eq!(hashed_uid.len(), 20);
     }
 }
